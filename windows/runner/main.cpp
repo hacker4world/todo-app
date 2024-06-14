9ef6dc608f1429d731269ceb7649bac591c38deb
hacker4world <zonegaming514@gmail.com>
2024-06-14 05:11:42 +0100

    .....

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -1,61 +1,43 @@
-// windows/runner/main.cpp
+#include <flutter/dart_project.h>
+#include <flutter/flutter_view_controller.h>
+#include <windows.h>
 
 #include "flutter_window.h"
 #include "utils.h"
 
-#include <flutter/flutter_view_controller.h>
-#include <windows.h>
-#include <iostream>
-
-#include <flutter/standard_method_codec.h>
-#include <flutter/method_channel.h>
-
-using namespace flutter;
-
-void RegisterCameraPlugin(BinaryMessenger* messenger) {
-  auto channel = std::make_unique<MethodChannel<EncodableValue>>(
-    messenger, "camera_plugin",
-    &StandardMethodCodec::GetInstance());
-
-  channel->SetMethodCallHandler(
-    [](const MethodCall<EncodableValue>& call, std::unique_ptr<MethodResult<EncodableValue>> result) {
-      if (call.method_name().compare("hasCamera") == 0) {
-        // Add your logic to check if camera is available
-        result->Success(EncodableValue(true));
-      } else if (call.method_name().compare("captureImage") == 0) {
-        // Add your code to capture an image using Windows API
-        // For example, you might use Media Foundation APIs here
-        // Return the path to the captured image or error
-        result->Success(EncodableValue("path/to/captured/image.jpg"));
-      } else {
-        result->NotImplemented();
-      }
-    });
-}
-
-int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev, _In_ wchar_t* command_line, _In_ int show_command) {
-  // Attach to console when present (e.g., 'flutter run') or create a new console when running with a debugger.
+int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
+                      _In_ wchar_t *command_line, _In_ int show_command) {
+  // Attach to console when present (e.g., 'flutter run') or create a
+  // new console when running with a debugger.
   if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
     CreateAndAttachConsole();
   }
 
-  // Initialize COM libraries
-  CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+  // Initialize COM, so that it is available for use in the library and/or
+  // plugins.
+  ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
 
-  // Create the Flutter window
-  FlutterWindow window(instance);
+  flutter::DartProject project(L"data");
+
+  std::vector<std::string> command_line_arguments =
+      GetCommandLineArguments();
+
+  project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
+
+  FlutterWindow window(project);
+  Win32Window::Point origin(10, 10);
+  Win32Window::Size size(1280, 720);
+  if (!window.Create(L"todo", origin, size)) {
+    return EXIT_FAILURE;
+  }
   window.SetQuitOnClose(true);
 
-  RegisterCameraPlugin(window.GetPlatformMessenger());
-
-  // Run the message loop
   ::MSG msg;
   while (::GetMessage(&msg, nullptr, 0, 0)) {
     ::TranslateMessage(&msg);
     ::DispatchMessage(&msg);
   }
 
-  // Uninitialize COM libraries
-  CoUninitialize();
+  ::CoUninitialize();
   return EXIT_SUCCESS;
 }
